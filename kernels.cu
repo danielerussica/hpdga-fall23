83fbb36fab359f85ffa09c121801b6ea5aa0ac15
Daniele Russica <daniele.russica@gmail.com>
2023-12-25 18:11:38 +0100

    removed not used files

--- kernels.cu
+++ kernels.cu
@@ -323,6 +323,8 @@ __global__ void get_min_intrablock6(const float* gpu_dist,
                                     const int  * gpu_index,
                                     int          offset,
                                     int          query_nb,
+                                    int          batch,
+                                    int          batches,
                                     float      * min_candidates,
                                     int        * min_index_candidates){
 
@@ -330,7 +332,7 @@ __global__ void get_min_intrablock6(const float* gpu_dist,
     // 2 * blockDim * sizeof(float) for distances and indexes
     extern __shared__ float smem[];
     
-    for(unsigned int query_index=0; query_index<query_nb; query_index++){
+    for(unsigned int query_index=batch*(query_nb/batches); query_index<query_nb/batches; query_index++){
         // copy distances and indexes to shared mem
         smem[threadIdx.x]            =  gpu_dist[(query_nb*blockDim.x*blockIdx.x)+(threadIdx.x*query_nb) + offset + query_index];
         smem[threadIdx.x+blockDim.x] = gpu_index[(query_nb*blockDim.x*blockIdx.x)+(threadIdx.x*query_nb) + offset + query_index];
@@ -410,179 +412,8 @@ __global__ void get_min_interblock6(const float * min_candidates,
 }
 
 
-// -- USED FOR PICK_K ON GPU W INNER LOOP--
-/*
-    * Find min using reduction, then set corresponding value to 1 in gpu_dist and repeat k times
-    * Handles distances and indexes
-    * The idea behind this part is to exploit the fact that ref_nb is a perfect square, so we can match intra and inter part using same amount of threads.
-    * Eg: ref_nb = 16384, we can have 128 threads per block and 128 blocks. And later block 0 will do final reduction on 128 values
-    * 99% working, issue is that accessing global memory is slow, so we need to find a way to reduce the number of accesses
-*/
-__global__ void get_min4(float      * gpu_dist,
-                         const int  * gpu_index,
-                         int          query_index,
-                         int          query_nb,
-                         float      * min_candidates,
-                         int        * min_index_candidates,
-                         int          i,                        // #ith min             
-                         float      * knn_dist,
-                         int        * knn_index){
 
-    // set up shared mem
-    // 2 * blockDim * sizeof(float) for distances and indexes
-    extern __shared__ float smem[];
-    
-    // Intrablock reduction phase
-
-    // copy distances and indexes to shared mem
-    smem[threadIdx.x] = gpu_dist[(query_nb*blockDim.x*blockIdx.x)+(threadIdx.x*query_nb)+query_index];
-    smem[threadIdx.x+blockDim.x] = gpu_index[(query_nb*blockDim.x*blockIdx.x)+(threadIdx.x*query_nb)+query_index];
-
-    __syncthreads();
-
-    // if(smem[threadIdx.x] == 0){
-    //     printf("smem[%d]: %f\n", threadIdx.x, smem[threadIdx.x]);
-    //     printf("smem[%d]: %f\n", threadIdx.x+blockDim.x, smem[threadIdx.x+blockDim.x]);
-    // }
-
-    // find min
-    for(unsigned int s=blockDim.x/2; s>0; s>>=1){
-        if(threadIdx.x < s){
-            if(smem[threadIdx.x] > smem[threadIdx.x + s]){
-                smem[threadIdx.x] = smem[threadIdx.x + s];
-                smem[threadIdx.x+blockDim.x] = smem[threadIdx.x + s + blockDim.x];
-            }
-        }
-        __syncthreads();
-    }
-
-    if (threadIdx.x == 0){
-        min_candidates[(blockIdx.x*query_nb)+query_index] = smem[0];
-        min_index_candidates[(blockIdx.x*query_nb)+query_index] = smem[blockDim.x];
-    }  
-
-    __syncthreads();
-
-    // Interblock reduction phase
-    if (blockIdx.x == 0){
-
-        // copy distances and indexes to shared mem
-        smem[threadIdx.x] = min_candidates[(threadIdx.x*query_nb)+query_index];
-        smem[threadIdx.x+blockDim.x] = min_index_candidates[(threadIdx.x*query_nb)+query_index];
-
-        // if(smem[threadIdx.x] == 0){
-        //     printf("smem[%d]: %f\n", threadIdx.x, smem[threadIdx.x]);
-        //     printf("smem[%d]: %f\n", threadIdx.x+blockDim.x, smem[threadIdx.x+blockDim.x]);
-        // }
-
-
-        __syncthreads();
-
-        // find min
-        for(unsigned int s=blockDim.x/2; s>0; s>>=1){
-            if(threadIdx.x < s){
-                if(smem[threadIdx.x] > smem[threadIdx.x + s]){
-                    smem[threadIdx.x] = smem[threadIdx.x + s];
-                    smem[threadIdx.x+blockDim.x] = smem[threadIdx.x + s + blockDim.x];
-                }
-            }
-            __syncthreads();
-        }
-
-        // write result for this block to global memory
-        if (threadIdx.x == 0){
-            int min_index = smem[blockDim.x];
-
-            // printf("placing %f\n", smem[0]);
-            knn_dist[(query_nb*i)+query_index] = smem[0];
-            knn_index[(query_nb*i)+query_index] = min_index;
-
-            // printf("placing %f at index: %d\n", smem[0], (k*query_index)+i);
-
-            // set gpu_dist["min_index"] to 1
-            gpu_dist[(min_index*query_nb)+query_index] = 1;
-
-        }
-
-        __syncthreads();
-
-    }
-
-}
-
-
-
-
-
-/**
- * Selection sort the distances stopping at K sorted elements
- * exploit mask: h_candidates
- */
-void selection_sort(float *dist, int *index, int *mask, int length, int k, int query_index, int query_nb, float *knn_dist, int *knn_index){
-
-    for(int i=0; i<k; i++){
-        float min_value = 1;
-        int min_index = 0;
-
-        for(int j=0; j<length; j++){
-            if(mask[(j*query_nb)+query_index] == 0){
-                continue;
-            }
-            if(dist[(j*query_nb)+query_index] < min_value){
-                min_value = dist[(j*query_nb)+query_index];
-                min_index = index[(j*query_nb)+query_index];
-            }
-        }
-
-        // place values in knn_dist and knn_index
-        knn_dist[(query_nb*i)+query_index] = min_value;
-        knn_index[(query_nb*i)+query_index] = min_index;
-
-        dist[(min_index*query_nb)+query_index] = 1;
-
-    }
-}
-
-
-/**
- * Sort the distances stopping at K sorted elements
- * CPU version
- */
-void  insertion_sort_on_matrix(float *dist, int *index, int length, int k, int query_index, int query_nb){
-
-    // Initialise the first index
-    index[0] = 0;
-
-    // Go through all points
-    for (int i=1; i<length; ++i) {
-
-        // Store current distance and associated index
-        float curr_dist  = dist[(i*query_nb)+query_index];
-        int   curr_index = i;
-
-        // Skip the current value if its index is >= k and if it's higher the k-th slready sorted mallest value
-        if (i >= k && curr_dist >= dist[(k-1)*query_nb+query_index]) {
-            continue;
-        }
-
-        // Shift values (and indexes) higher that the current distance to the right
-        int j = min(i, k-1);
-        while (j > 0 && dist[(j-1)*query_nb+query_index] > curr_dist) {
-            dist[(j*query_nb)+query_index]  = dist[((j-1)*query_nb)+query_index];
-            index[(j*query_nb)+query_index] = index[((j-1)*query_nb)+query_index];
-            --j;
-        }
-
-        // Write the current distance and index at their position
-        dist[(j*query_nb)+query_index]  = curr_dist;
-        index[(j*query_nb)+query_index] = curr_index; 
-    }
-}
-
-// The idea is to use a custom insertion sort that will only sort the k first elements on gpu where each thread will sort a query
-// Since we have more than 1024 queries, we need to use several blocks to handle all queries
-
-__global__ void gpu_custom_insertion_sort(float *dist, int *index, int length, int k, int query_nb, int offset, float *knn_dist, int *knn_index){
+__global__ void gpu_custom_insertion_sort(float *dist, int *index, int length, int k, int query_nb, int batches, int offset, float *knn_dist, int *knn_index){
     
     int tid = (blockIdx.x * blockDim.x) + threadIdx.x;
 
@@ -616,7 +447,7 @@ __global__ void gpu_custom_insertion_sort(float *dist, int *index, int length, i
 
     // Copy the k smallest distances and associated index to the knn_dist and knn_index arrays
     for (int i = 0; i < k; i++) {
-        knn_dist[(query_nb * i) + tid + offset] = dist[(i * query_nb) + tid];
-        knn_index[(query_nb * i) + tid + offset] = index[(i * query_nb) + tid];
+        knn_dist[(query_nb*batches*i)+tid+offset] = dist[(i * query_nb) + tid];
+        knn_index[(query_nb*batches*i)+tid+offset] = index[(i * query_nb) + tid];
     }
 }
