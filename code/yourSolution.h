1baf284bded1c54b9f5fa8a111bc3d95a24e5e54
Daniele Russica <daniele.russica@gmail.com>
2023-12-26 10:32:54 +0100

    change solution function name

--- code/yourSolution.h
+++ code/yourSolution.h
@@ -7,7 +7,7 @@ bool your_solution(const float * ref,
                      float *       knn_dist,
                      int *         knn_index);
 
-bool ys_gpu_partial_sort2(const float * ref,
+bool your_solution2(const float * ref,
                      int           ref_nb,
                      const float * query,
                      int           query_nb,
